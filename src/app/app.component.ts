cdd1104ef14be60608fdf4fb07ca0413fc1705f1
akash.majida <akash.majida@gmail.com>
2024-09-06 11:16:13 +0000

    Tried to create sign up component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+
+import{NzSpinModule} from 'ng-zorro-antd/spin';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzSpinModule, NzLayoutModule, NzMenuModule, RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'myapp';
